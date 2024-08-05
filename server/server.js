490badefedd4879a460a7f302a27982728eb1bc6
lakshya dadhich <laskhyadadhich175@gmail.com>
2024-08-05 09:01:41 +0530

    edit the code

--- server/server.js
+++ server/server.js
@@ -1,73 +1,75 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const bcryptjs = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+  const express = require("express");
+  const mongoose = require("mongoose");
+  const cors = require("cors");
+  const bcryptjs = require("bcryptjs");
+  const jwt = require("jsonwebtoken");
+  require('dotenv').config();
 
-const PORT = 5000;
-const app = express();
-const MONGODB_URI = "mongodb+srv://lakshyadadhich175:lakshya1234@cluster0.jaf6rwj.mongodb.net/auth-db?retryWrites=true&w=majority&appName=Cluster0";
 
-app.use(cors());
-app.use(express.json());
+  const PORT = 5000;
+  const app = express();
+  const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
+  app.use(cors());
+  app.use(express.json());
 
-db.on("error", (err) => {
-  console.error("Mongodb connection error", err);
-});
+  mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  const db = mongoose.connection;
 
-db.once("open", () => {
-  console.log("Mongodb is connected");
-});
+  db.on("error", (err) => {
+    console.error("Mongodb connection error", err);
+  });
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-});
+  db.once("open", () => {
+    console.log("Mongodb is connected");
+  });
 
-const User = mongoose.model("User", userSchema);
+  const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+  });
 
-app.post("/register", async (req, res) => {
-  try {
-    const hashedPassword = await bcryptjs.hash(req.body.password, 10);
-    const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword,
-    });
-    const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
-  } catch (error) {
-    console.error("Error during registration", error);
-    res.status(500).json({ error: "internal server error" });
-  }
-});
+  const User = mongoose.model("User", userSchema);
 
-app.post("/login", async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
+  app.post("/register", async (req, res) => {
+    try {
+      const hashedPassword = await bcryptjs.hash(req.body.password, 5);
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+      });
+      const savedUser = await newUser.save();
+      res.status(201).json(savedUser);
+    } catch (error) {
+      console.error("Error during registration", error);
+      res.status(500).json({ error: "internal server error" });
     }
+  });
 
-    const isPasswordValid = await bcryptjs.compare(req.body.password, user.password);
-    if (!isPasswordValid) {
-      return res.status(401).json({ error: "Invalid password" });
+  app.post("/login", async (req, res) => {
+    try {
+      const user = await User.findOne({ email: req.body.email });
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+
+      const isPasswordValid = await bcryptjs.compare(req.body.password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ error: "Invalid password" });
+      }
+
+    
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
+
+      res.status(200).json({ message: "Login successful", token });
+    } catch (error) {
+      console.error("Error during login", error);
+      res.status(500).json({ error: "internal server error" });
     }
+  });
 
-   
-    const token = jwt.sign({ id: user._id }, 'your_jwt_secret_key', { expiresIn: '1h' });
-
-    res.status(200).json({ message: "Login successful", token });
-  } catch (error) {
-    console.error("Error during login", error);
-    res.status(500).json({ error: "internal server error" });
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
